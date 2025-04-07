8aee07d7b095786bff6f0c97c06d199e19aa43b9
Lucas Gabriel <lucas.gah2000@gmail.com>
2025-04-07 12:54:44 -0300

    Atualizar o clienteRoutes.js

--- backend/routes/clienteRoutes.js
+++ backend/routes/clienteRoutes.js
@@ -1,26 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const Cliente = require('../models/Cliente');
+const clienteController = require('../controllers/clienteController');
 
-// Criar cliente
-router.post('/', async (req, res) => {
-  try {
-    const novoCliente = new Cliente(req.body);
-    await novoCliente.save();
-    res.status(201).json(novoCliente);
-  } catch (err) {
-    res.status(400).json({ erro: err.message });
-  }
-});
-
-// Listar clientes
-router.get('/', async (req, res) => {
-  try {
-    const clientes = await Cliente.find();
-    res.json(clientes);
-  } catch (err) {
-    res.status(500).json({ erro: err.message });
-  }
-});
+router.get('/', clienteController.listar);
+router.post('/', clienteController.criar);
+router.put('/:id', clienteController.atualizar);
+router.delete('/:id', clienteController.excluir);
 
 module.exports = router;
