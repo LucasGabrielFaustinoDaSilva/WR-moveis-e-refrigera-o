5f2a399dbb4423ab3ef6e5d8858b75bd676b6fca
Lucas Gabriel <lucas.gah2000@gmail.com>
2025-04-11 12:33:00 -0300

    Atualizar o db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,16 @@
 const mysql = require('mysql2');
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 
-const pool = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
 });
 
-module.exports = pool.promise();
+db.connect(err => {
+  if (err) throw err;
+  console.log('Conectado ao MySQL');
+});
+
+module.exports = db;
